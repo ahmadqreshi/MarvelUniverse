c6f1d2ea8ce5315709002637138028eeba586c2c
Ahmad Qureshi <ahmadqureshi40@gmail.com>
2023-10-10 01:12:45 +0530

    initial Commit

---
+++ MarvelUniverse/MarvelUniverseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelUniverseApp.swift
+//  MarvelUniverse
+//
+//  Created by Ahmad Qureshi on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MarvelUniverseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
