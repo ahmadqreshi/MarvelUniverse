8a0abcf484dedbfe4c1f81c90a74fda22d23790c
Ahmad Qureshi <ahmadqureshi40@gmail.com>
2023-10-10 23:03:29 +0530

    add custom loader

---
+++ MarvelUniverse/Utility/CustomView/LoaderView.swift
@@ -0,0 +1,25 @@
+//
+//  LoaderView.swift
+//  MarvelUniverse
+//
+//  Created by Ahmad Qureshi on 10/10/23.
+//
+
+import SwiftUI
+
+struct LoaderView: View {
+    var body: some View {
+        ZStack {
+            ProgressView()
+                .progressViewStyle(.circular)
+                .tint(.red)
+                .controlSize(.large)
+        }
+    }
+}
+
+struct LoaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoaderView()
+    }
+}
